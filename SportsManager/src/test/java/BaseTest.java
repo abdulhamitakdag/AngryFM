f1993d75ef263495afbf1be2e5c389e2bab052c5
S. Gökçe <gokcegok2004@gmail.com>
2026-03-30 23:54:25 +0300

    BaseTest abstract class written, TestingZone changed to be TestObjFactory for ease of coding as team.

--- SportsManager/src/test/java/BaseTest.java
+++ SportsManager/src/test/java/BaseTest.java
@@ -1,6 +1,17 @@
-public class BaseTest {
+package com.sportsmanager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+public abstract class BaseTest {
     /*arkadaşlar bu class testing yaparken Maven'ın okuduğu kısım*/
     /*test metodlarını buradan okuyup otomatik çalıştırıyor */
     /*diğer bütün test classları bu classı extendlicek sooo bu class abstract olacak*/
 
+    @BeforeEach void init(){}
+    protected void assertWithinRange(double value, double min, double max, String name) {
+        if (value < min || value > max) {
+            throw new AssertionError(name + " was " + value + ", but expected between " + min + " and " + max);
+        }
+    }
 }
