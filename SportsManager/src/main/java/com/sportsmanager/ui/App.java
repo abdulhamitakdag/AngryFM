dd37346fff2bfa39c8f3fb0d6907f490cb014bdf
Abdulhamit Akdağ <akdagabdulhamit6@gmail.com>
2026-04-28 21:26:12 +0300

    improvements on MainMenuView.fxml

--- SportsManager/src/main/java/com/sportsmanager/ui/App.java
+++ SportsManager/src/main/java/com/sportsmanager/ui/App.java
@@ -2,21 +2,26 @@ package com.sportsmanager.ui;
 
 import javafx.application.Application;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
+import javafx.fxml.FXMLLoader;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception{
 
-        Group root =new Group();
-        Scene scene =new Scene(root,400,500, Color.AQUA);
+        java.net.URL fxmlLocation =getClass().getResource("/MainMenuView.fxml");
+        if (fxmlLocation==null){
+            throw  new IllegalStateException("fxml file has not been found");
+        }
+        Parent root = FXMLLoader.load(fxmlLocation);
+        Scene scene =new Scene(root);
         stage.setTitle("ANGRY BIRDS");
         java.io.InputStream iconstream =getClass().getResourceAsStream("/icon.png");
         if (iconstream!=null){
@@ -28,6 +33,8 @@ public class App extends Application {
 
         stage.setHeight(720);
         stage.setWidth(900);
+        stage.setMinHeight(300);
+        stage.setMinWidth(600);
         // stage.setFullScreen(true); this might be optinal
        // stage.setFullScreenExitHint("If the full size is not convinient,\nPress escape");
         stage.setScene(scene);
